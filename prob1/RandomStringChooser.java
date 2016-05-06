e883b532993856e3c69e99a5484ec1cab6db800c
iProdigy <prodigycreep@gmail.com>
2016-05-05 19:31:02 -0500

    Use Math#random rather than java.util.Random

--- prob1/RandomStringChooser.java
+++ prob1/RandomStringChooser.java
@@ -3,13 +3,11 @@ package prob1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Problem 1.a
  */
 public class RandomStringChooser {
-    private static final Random rand = new Random();
     private final List<String> words;
 
     public RandomStringChooser(String[] wordArray) {
@@ -23,7 +21,7 @@ public class RandomStringChooser {
      */
     public String getNext() {
         final int n = words.size();
-        return (n == 0) ? "NONE" : words.remove(rand.nextInt(n));
+        return (n == 0) ? "NONE" : words.remove((int) (Math.random() * n));
     }
 
     /**
